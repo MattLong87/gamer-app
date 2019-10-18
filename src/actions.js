ac3ad2225a720af40854e819ede87eb8e37c6571
Matthew Long <MattLong87@gmail.com>
2019-10-17 21:21:45 -0400

    started to connect redux to api, getting bad request error though

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,43 @@
+import {API_BASE_URL} from './config';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from "graphql-tag";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: API_BASE_URL + 'graphql'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
 export const ADD_PROFILE = 'ADD_PROFILE';
 export const addProfile = () => ({
     type: ADD_PROFILE
 });
+
+export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+export const fetchDataSuccess = data => ({
+    type: FETCH_DATA_SUCCESS,
+    data
+});
+
+export const fetchData = () => dispatch => {
+    client
+    .query({
+      query: gql`
+        query GetLaunch {
+          launch(id: 56) {
+            id
+            mission {
+              name
+            }
+          }
+        }
+      `
+    })
+    .then(result => console.log(result));
+};
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {connect} from 'react-redux';
+import { Link } from "react-router-dom";
 import Thumbnail from './Thumbnail';
 import '../css/grid.css';
 
-export default class Grid extends React.Component {
+import {fetchData} from '../actions';
+
+export class Grid extends React.Component {
+
+  componentDidMount() {
+    this.props.dispatch(fetchData());
+  }
 
   render() {
-    
+
     let thumbnails = [];
-    
-    this.props.profiles.forEach(profile => thumbnails.push(<Link to={{pathname: "/profile", state: {profile: profile}}}><Thumbnail profile={profile} /></Link>));
+
+    this.props.profiles.forEach(profile => thumbnails.push(<Link to={{ pathname: "/profile", state: { profile: profile } }}><Thumbnail profile={profile} /></Link>));
 
     return (
       <div className="grid">
-          {thumbnails}
-          <Link to="/editProfile" className="edit-profile-button">
-            Edit Profile
+        {thumbnails}
+        <Link to="/editProfile" className="edit-profile-button">
+          Edit Profile
           </Link>
       </div>
     );
   }
 }
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+export default connect(mapStateToProps)(Grid);
