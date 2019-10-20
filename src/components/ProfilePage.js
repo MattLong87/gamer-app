b8201b0699ba69d0f5f8f234431b982dcf1b7444
Matthew Long <MattLong87@gmail.com>
2019-10-20 17:04:29 -0400

    completed initial styling of profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -7,7 +7,8 @@ export default class ProfilePage extends React.Component {
         let profile = this.props.location.state.profile;
 
         return (
-            <div className="profilePage" style={{backgroundImage: 'url(' + profile.photo + ')'}}>
+            // gradient below looks crazy because it's an eased gradient. details: https://larsenwork.com/easing-gradients/
+            <div className="profilePage" style={{backgroundImage: 'linear-gradient(to top,hsla(0, 0%, 0%, 0.75) 0%,hsla(0, 0%, 0%, 0.61) 7.2%,hsla(0, 0%, 0%, 0.488) 12.6%,hsla(0, 0%, 0%, 0.384) 16.5%,hsla(0, 0%, 0%, 0.296) 19.5%,hsla(0, 0%, 0%, 0.222) 21.9%,hsla(0, 0%, 0%, 0.162) 24.1%,hsla(0, 0%, 0%, 0.114) 26.5%,hsla(0, 0%, 0%, 0.076) 29.5%,hsla(0, 0%, 0%, 0.048) 33.4%,hsla(0, 0%, 0%, 0.028) 38.7%,hsla(0, 0%, 0%, 0.014) 45.9%,hsla(0, 0%, 0%, 0.006) 55.1%,hsla(0, 0%, 0%, 0.002) 67%,hsla(0, 0%, 0%, 0) 81.8%,hsla(0, 0%, 0%, 0) 100%), url(' + profile.photo + ')'}}>
                 <Link to="/">
                     <button className="back">Back</button>
                 </Link>
@@ -19,7 +20,7 @@ export default class ProfilePage extends React.Component {
                 <span className="favorite-games">{profile.favoriteGames}</span>
                 <label className="want-to-play-label">Want To Play</label>
                 <span className="want-to-play">{profile.wantToPlay}</span>
-                <Link to="/chat">
+                <Link className="chat-link" to="/chat">
                     <button className="chat-button">Chat</button>
                 </Link>
             </div>
