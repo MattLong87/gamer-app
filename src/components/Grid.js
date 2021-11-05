10a0f64b566cb5bfb796501b55fd919e67ef1fd6
Matt Long <mattlong87@gmail.com>
2021-11-05 14:25:31 -0400

    sorted grid by distance

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -16,16 +16,18 @@ export class Grid extends React.Component {
 
     let thumbnails = [];
 
-    if(this.props.data){
-      this.props.data.allPersons.forEach(profile => thumbnails.push(<Link to={{ pathname: "/profile", state: { profile: profile } }} key={profile.id}><Thumbnail profile={profile} /></Link>));
+    if (this.props.data) {
+      this.props.data.allPersons
+        .sort((profile1, profile2) => { return parseInt(profile1.distance.split(' ')[0]) - parseInt(profile2.distance.split(' ')[0]) })
+        .forEach(profile => thumbnails.push(<Link to={{ pathname: "/profile", state: { profile: profile } }} key={profile.id}><Thumbnail profile={profile} /></Link>));
     }
-    
+
     return (
       <div className="grid">
         {thumbnails}
         <Link to="/editProfile" className="edit-profile-button">
           Edit Profile
-          </Link>
+        </Link>
       </div>
     );
   }
